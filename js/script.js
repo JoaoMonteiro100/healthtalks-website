19e5a25d4f96fa119dbba5adceb0640b65c72006
João Monteiro <joaomiguelmonteiro@gmail.com>
2017-12-06 10:39:09 +0000

    Added news section

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $('.carousel').carousel({
 
 $(document).ready(function(){
     var scrollTop = 0;
+    document.getElementById("news-articles").style.display = "none";
     $(window).scroll(function(){
         scrollTop = $(window).scrollTop();
         $('.counter').html(scrollTop);
@@ -27,3 +28,16 @@ $('.launch-modal').on('click', function(e){
         $iframe.src = $iframe.src;
     });
 });
+
+function toggleArticles() {
+    var articles = document.getElementById("news-articles");
+    var icons = document.getElementById("news-block");
+    if(articles.style.display === "none") {
+        articles.style.display = "block";
+        icons.style.display = "none";
+    }
+    else {
+        articles.style.display = "none";
+        icons.style.display = "block";
+    }
+}
